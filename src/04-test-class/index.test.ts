1d4ca63db5fd589f19b7c9ca4aed38e702a6be7a
Falderian <nikitastaselovich@gmail.com>
2023-07-14 08:58:56 +0300

    her

--- src/04-test-class/index.test.ts
+++ src/04-test-class/index.test.ts
@@ -1,33 +1,52 @@
 // Uncomment the code below and write your tests
-// import { getBankAccount } from '.';
+import { InsufficientFundsError, TransferFailedError, getBankAccount } from '.';
 
 describe('BankAccount', () => {
+  const initialValue = 1000;
+
+  const newAccount = getBankAccount(initialValue);
+
+  const secondAccount = getBankAccount(initialValue);
   test('should create account with initial balance', () => {
-    // Write your test here
+    expect(newAccount.getBalance()).toEqual(initialValue);
   });
 
   test('should throw InsufficientFundsError error when withdrawing more than balance', () => {
-    // Write your test here
+    try {
+      newAccount.withdraw(1005);
+    } catch (e) {
+      expect(e).toBeInstanceOf(InsufficientFundsError);
+    }
   });
 
   test('should throw error when transferring more than balance', () => {
-    // Write your test here
+    try {
+      newAccount.transfer(1005, secondAccount);
+    } catch (e) {
+      expect(e).toBeInstanceOf(InsufficientFundsError);
+    }
   });
 
   test('should throw error when transferring to the same account', () => {
-    // Write your test here
+    try {
+      newAccount.transfer(1005, newAccount);
+    } catch (e) {
+      expect(e).toBeInstanceOf(TransferFailedError);
+    }
   });
 
   test('should deposit money', () => {
-    // Write your test here
+    expect(newAccount.deposit(100).getBalance()).toEqual(1100);
   });
 
   test('should withdraw money', () => {
-    // Write your test here
+    expect(newAccount.withdraw(100).getBalance()).toEqual(1000);
   });
 
   test('should transfer money', () => {
-    // Write your test here
+    newAccount.transfer(100, secondAccount);
+    expect(newAccount.getBalance()).toEqual(900);
+    expect(secondAccount.getBalance()).toEqual(1100);
   });
 
   test('fetchBalance should return number in case if request did not failed', async () => {
